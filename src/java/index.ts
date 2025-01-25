e7a075c6515f8379eb67a6bc51f9142b19790b0e
kage1020 <4624528@ed.tus.ac.jp>
2025-01-25 15:03:46 +0900

    remove version union types

--- src/java/index.ts
+++ src/java/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import { Versions } from '../const/version';
+import versions from '../const/version.json';
 import v1 from './1.0';
 
 const app = new Hono();
@@ -7,7 +7,15 @@ const app = new Hono();
 app.route('/1.0', v1);
 
 app.get('/', (c) => {
-  return c.json({ routes: Versions });
+  return c.json({ routes: versions.major });
+});
+
+app.get('/version', (c) => {
+  return c.json({ versions });
+});
+
+app.get('/snapshot', (c) => {
+  return c.json({ snapshots: versions.snapshot });
 });
 
 export default app;
